e061a260363ea87f4635752e97c4460e3e938d47
KAIY\31207 <3120713254@qq.com>
2025-06-10 11:22:12 +0800

    新增了Boss模型，扩大了场景大小，新增boss地盘，优化Npc对话系统，引入新的Npc

---
+++ 九州浮生录Beta0.3/Assets/Scripts/Npc/LeiShen.cs
@@ -0,0 +1,14 @@
+/****************************************************
+    文件：LeiShen.cs
+	作者：Kaiy
+    邮箱: 3120713254@qq.com
+    日期：#CreateTime#	
+	功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+
+public class LeiShen : NpcDialogue
+{
+
+}
---
+++ 九州浮生录Beta0.3/Assets/Scripts/Npc/SiHa.cs
@@ -0,0 +1,14 @@
+/****************************************************
+    文件：SiHa.cs
+	作者：Kaiy
+    邮箱: 3120713254@qq.com
+    日期：#CreateTime#	
+	功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+
+public class SiHa : MonoBehaviour 
+{
+
+}
